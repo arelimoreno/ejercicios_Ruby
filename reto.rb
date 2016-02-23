f4783705a99356f3c79f093500609c6a873ba5bf
arelimoreno <areli.94moreno@gmail.com>
2016-02-23 06:39:20 -0500

    final

--- reto.rb
+++ reto.rb
@@ -1,22 +1,22 @@
 for numero in 1..10000
+	suma1=0
+	suma2=0
     for i in 1..numero
-    	suma1=0
 		if numero%i==0
 		suma1=suma1+i 
 		end
      end
-unless suma1==i
+ unless i==suma1
     for h in 1..suma1
-    	suma2=0
 		if suma1%h==0
 		suma2=suma2+h 
 		end
     end
-end
-end
- if suma2==numero
-	print "son numeros amigos:\n"	
-	puts suma2	
-else
+    if suma2==i
+	print "son numeros amigos:\n"
+	puts " #{suma1} y #{suma2}"	
+    else
 	print "no hay numeros amigos:\n"
+	    end
+ end
 end
